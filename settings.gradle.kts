5e2860d0287bd3ee4d2879dcdec8ec7ba21a7141
denisquaglia-app <denis.quaglia@rigola.it>
2026-03-29 00:48:37 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,17 @@
-rootProject.name = "CalorieFoto"
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Calorie Foto"
 include(":app")
