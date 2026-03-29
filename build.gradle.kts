029cbaf3a2741257236b99b97215d2e50d3f29c7
denisquaglia-app <denis.quaglia@rigola.it>
2026-03-29 01:11:44 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,7 @@ plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
+dependencies {
+    classpath("com.android.tools.build:gradle:8.1.4")
+    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
+}
