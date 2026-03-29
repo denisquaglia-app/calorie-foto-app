e919ebe14b07b8c85fa975baaa769bb573567460
denisquaglia-app <denis.quaglia@rigola.it>
2026-03-29 03:08:44 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,14 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
 
-tasks.register<Delete>("clean") {
-    delete(layout.buildDirectory)
-}
+android {
+    namespace = "com.denisq.caloriefoto"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.denisq.caloriefoto"
+        minSdk = 21
+        targetSdk = 34
+        versionCode
